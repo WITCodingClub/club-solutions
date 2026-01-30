c307ae6cf159fdd5f70ce857dd238ee9a65d120e
Daniel Miretsky <miretskyd@wit.edu>
2026-01-30 12:35:08 -0500

    Add my solution for AOC 2023 day 1

---
+++ 2026-01-13/Daniel Miretsky/Day1Part2.java
@@ -0,0 +1,22 @@
+package _2023.day01;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Day1Part2
+{
+	public static void main(String[] args)
+	{
+		//try (Scanner input = new Scanner(new File("./src/main/java/day01/input.txt")))
+		try (Scanner input = new Scanner(new File("./src/main/java/day01/input_test.txt")))
+		{
+			
+		}
+		catch (FileNotFoundException ex)
+		{
+			System.out.println("Error! File not found!");
+		}
+	}
+}
